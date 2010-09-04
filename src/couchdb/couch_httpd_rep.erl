8692e25e2a941df1630c6152674255b0eaa20f8a
Filipe David Borba Manana <fdmanana@apache.org>
2010-09-04 13:58:54 +0000

    New replicator: clean up - don't import functions that aren't used.
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/branches/new_replicator@992603 13f79535-47bb-0310-9956-ffa450edef68

--- src/couchdb/couch_httpd_rep.erl
+++ src/couchdb/couch_httpd_rep.erl
@@ -16,13 +16,9 @@
 -include("couch_api_wrap.hrl").
 
 -import(couch_httpd, [
-    send_json/2, send_json/3, send_json/4,
-    send/2,
-    send_method_not_allowed/2,
-    start_response_length/4,
-    start_json_response/2, start_json_response/3, end_json_response/1,
-    start_chunked_response/3, send_chunk/2, last_chunk/1,
-    absolute_uri/2
+    send_json/2,
+    send_json/3,
+    send_method_not_allowed/2
 ]).
     
 -export([handle_req/1]).
