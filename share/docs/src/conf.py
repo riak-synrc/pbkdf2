a72bde755245423b4b4815ecddc4887dcbb08b26
Noah Slater <nslater@apache.org>
2012-09-29 17:10:14 +0100

    created new config file from scratch

---
+++ share/docs/src/conf.py
@@ -0,0 +1,33 @@
+import sys, os
+
+extensions = ["sphinx.ext.todo"]
+
+source_suffix = ".rst"
+
+master_doc = "index"
+
+nitpicky = True
+
+project = "@@"
+
+copyright = "@@"
+
+version = "@@"
+
+release = "@@"
+
+highlight_language = "json"
+
+pygments_style = "sphinx"
+ 
+# @@ design CouchDB theme
+
+html_theme = "default"
+
+html_short_title = "@@"
+
+html_logo = "@@"
+
+html_favicon = "@@"
+
+html_use_opensearch = "@@"
